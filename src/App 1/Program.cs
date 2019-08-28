c48946c180764730fd4b23ec48e703ad88badb09
Aaron Pearson <aaron.2002@outlook.com>
2019-08-28 19:17:05 +0100

    Cleanup and update .gitignore file

--- src/App 1/Program.cs
+++ src/App 1/Program.cs
@@ -1,49 +1,77 @@
 ﻿using System;
 using System.Drawing;
-using System.Threading;
 using System.Threading.Tasks;
 using Silk.NET.Input;
+using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 using Silk.NET.Windowing.Common;
 
 namespace App_1
 {
-    class Program
+    static class Program
     {
-        private static CancellationTokenSource _tokenSource = new CancellationTokenSource();
-        private static CancellationToken _token => _tokenSource.Token;
         private static IWindow window;
-        
-        static async Task Main(string[] args)
+        private static readonly GL GL = GL.GetApi();
+        private static readonly Random Random = new Random();
+
+        private static async Task Main()
         {
-            window = Window.Create(WindowOptions.Default);
-            window.Size = new Size(10, 10);
-            window.WindowBorder = WindowBorder.Fixed;
-            window.Title = "Wew";
-
-            window.Render += d => render(d);
-            window.Closing += async () =>
-            {
-                window.Close();
-                while (window.IsVisible)
-                {
-                    await Task.Delay(1);
-                }
-
-                _tokenSource.Cancel();
-            };
-            
-            Task.Run(() => window.Run());
-            
-            window.GetInput().Keyboards[0].KeyDown += (keyboard, key) => { Console.WriteLine($"Down: {key}"); };
-            window.GetInput().Keyboards[0].KeyUp += (keyboard, key) => { Console.WriteLine($"Up: {key}"); };
-
-            await Task.Delay(-1, _token);
+            (await MakeWindow(false)).Run();
         }
 
-        private static async Task render(double deltaTime)
+        private static void Window_Move(Point obj)
         {
-            window.Size = Size.Add(window.Size, new Size(1,1));
+            Console.WriteLine($"Move: {obj.X}, {obj.Y}");
+        }
+
+        private static void Window_Update(double deltaTime)
+        {
+            // Change the screen colour
+            GL.ClearColor(Color.FromArgb(10, Random.Next(255), Random.Next(255), Random.Next(255)));
+            Console.WriteLine($"Update: {deltaTime}");
+        }
+
+        private static void Window_Load(IWindow window)
+        {
+            var keyboard = window.GetInput().Keyboards[0];
+            keyboard.KeyDown += (_, key) => { Console.WriteLine($"Down: {key}"); };
+            keyboard.KeyUp += (_, key) => { Console.WriteLine($"Up: {key}"); };
+
+            GL.ClearColor(Color.Chocolate);
+        }
+
+        private static void Window_Render(double deltaTime)
+        {
+            GL.Clear((uint)GLEnum.ColorBufferBit);
+            Console.WriteLine($"Render: {deltaTime}");
+
+            //window.Size = Size.Add(window.Size, new Size(1,1));
+        }
+
+        public static async Task<IWindow> MakeWindow(bool runWindow = true)
+        {
+            //new WindowOptions(true, false, new Point(0, 0), new Size(250, 500), 67, 67, GraphicsAPI.Default, "Wew", WindowState.Normal, WindowBorder.Resizable, VSyncMode.Adaptive, 30)
+            window = Window.Create(WindowOptions.Default);
+            window.Size = new Size(500, 250);
+            window.WindowBorder = WindowBorder.Resizable;
+            window.Title = "Wew";
+
+            window.Load += async () => 
+            {
+                Window_Load(window);
+                await Task.Delay(5000);
+                MakeWindow();
+            };
+            window.Update += Window_Update;
+            window.Render += Window_Render;
+            window.Move += Window_Move;
+
+            if (runWindow)
+            {
+                _ = Task.Run(() => window.Run()).ConfigureAwait(false);
+            }
+
+            return window;
         }
     }
 }
